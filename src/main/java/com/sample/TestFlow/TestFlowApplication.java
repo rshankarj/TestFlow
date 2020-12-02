88bcda6d02b5d6c03140ff31fcb3904b5f48c8a2
rshankarj@gmail.com <rshankarj@gmail.com>
2020-12-03 00:40:38 +0530

    Initial commit

---
+++ src/main/java/com/sample/TestFlow/TestFlowApplication.java
@@ -0,0 +1,13 @@
+package com.sample.TestFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFlowApplication.class, args);
+	}
+
+}
